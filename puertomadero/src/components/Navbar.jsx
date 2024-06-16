f3c05ab8b7d19412a09aeca6b9418a47342c5468
MathiasMendoza <mathiasmendoza_vargas14@hotmail.com>
2024-06-16 16:38:38 -0700

    adding custom navbar bg

--- puertomadero/src/components/Navbar.jsx
+++ puertomadero/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-gray-900 shadow-lg fixed w-full z-50">
+    <nav className="bg-[#001540] shadow-lg fixed w-full z-50">
       <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           {/* Navbar branding */}
