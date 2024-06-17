28f1018f3d2880d6a24644a8d89ccd9a402576d2
MathiasMendoza <mathiasmendoza_vargas14@hotmail.com>
2024-06-16 17:27:48 -0700

    improving mobile nav

--- puertomadero/src/components/Navbar.jsx
+++ puertomadero/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import '../styles/Navbar.css'; // Importing custom CSS file
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,7 +21,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-[#001540] shadow-lg fixed w-full z-50">
+    <nav className="bg-[#001540] shadow-lg fixed w-full z-50 font-body">
       <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           {/* Navbar branding */}
@@ -34,11 +33,11 @@ const Navbar = () => {
               className="flex items-center"
             >
               <img
-                className="block h-8 sm:h-10 w-auto"
+                className="block h-8 sm:h-10 w-auto rounded-full"
                 src="/Sources/logo.jpg"
                 alt="Logo"
               />
-              <span className="ml-2 text-white text-lg font-semibold">
+              <span className="ml-2 text-white text-lg font-bold sm:text-xl">
                 Puerto Madero Urubó
               </span>
             </a>
@@ -49,43 +48,43 @@ const Navbar = () => {
             <div className="flex space-x-4">
               <a
                 href="#Promoción"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm sm:text-base font-medium"
               >
                 Promoción
               </a>
               <a
                 href="#avancesPavimentacion"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm sm:text-base font-medium"
               >
                 Pavimentación
               </a>
               <a
                 href="#avancesCaballeriza"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm sm:text-base font-medium"
               >
                 Caballeriza
               </a>
               <a
                 href="#ContaráCon"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm sm:text-base font-medium"
               >
                 Contará Con
               </a>
               <a
                 href="#Serviciosdelujo"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm sm:text-base font-medium"
               >
                 Servicios de Lujo
               </a>
               <a
                 href="#ubicacion"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm sm:text-base font-medium"
               >
                 Ubicación
               </a>
               <a
                 href="#Serviciosbasicos"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm sm:text-base font-medium"
               >
                 Servicios Básicos
               </a>
